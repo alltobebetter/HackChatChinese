908fff2ab62b0f46efde1395c0e65f3e6bf9ca74
alltobebetter <alltobebetter@outlook.com>
2024-06-13 07:26:38 +0000

    Initial commit

--- client/client.js
+++ client/client.js
@@ -1,13 +1,13 @@
 /*
  *
- * NOTE: The client side of hack.chat is currently in development,
- * a new, more modern but still minimal version will be released
- * soon. As a result of this, the current code has been deprecated
- * and will not actively be updated.
+ * 注意：hack.chat 的客户端目前正在开发中，
+ * 一个更新、更现代但仍然简洁的版本将很快发布。
+ * 因此，当前代码已被弃用，
+ * 并且不会被积极更新。
  *
 */
 
-//select "chatinput" on "/"
+// 当按下"/"键时，选中"chatinput"
 document.addEventListener("keydown", e => {
     if (e.key === '/' && document.getElementById("chatinput") != document.activeElement) {
         e.preventDefault();
@@ -15,7 +15,7 @@ document.addEventListener("keydown", e => {
     }
 });
 
-// initialize markdown engine
+// 初始化 Markdown 引擎
 var markdownOptions = {
 	html: false,
 	xhtmlOut: false,
@@ -46,7 +46,7 @@ var markdownOptions = {
 
 var md = new Remarkable('full', markdownOptions);
 
-// image handler
+// 图片处理器
 var allowImages = false;
 var imgHostWhitelist = [
 	'i.imgur.com',
@@ -106,7 +106,7 @@ md.use(remarkableKatex);
 function verifyLink(link) {
 	var linkHref = Remarkable.utils.escapeHtml(Remarkable.utils.replaceEntities(link.href));
 	if (linkHref !== link.innerHTML) {
-		return confirm('Warning, please verify this is where you want to go: ' + linkHref);
+		return confirm('警告，请确认这是否是您要访问的链接: ' + linkHref);
 	}
 
 	return true;
@@ -123,31 +123,31 @@ var frontpage = [
 	"                           |_|_|__/|___|_,_|.|___|_|_|__/|_|  ",
 	"",
 	"",
-	"Welcome to hack.chat, a minimal, distraction-free chat application.",
-	"Channels are created, joined and shared with the url, create your own channel by changing the text after the question mark.",
-	"If you wanted your channel name to be 'your-channel': https://hack.chat/?your-channel",
-	"There are no channel lists, so a secret channel name can be used for private discussions.",
+	"欢迎来到 hack.chat，一个简洁、无干扰的聊天应用。",
+	"您可以通过修改问号后的文字来创建、加入和分享频道。",
+	"例如，如果您想要创建一个名为“您的频道”的频道，链接应该是：https://hack.chat/?您的频道",
+	"由于没有频道列表，因此您可以使用秘密频道名称进行私人讨论。",
 	"",
-	"Here are some pre-made channels you can join:",
+	"以下是一些预先创建的频道，您可以加入：",
 	"?lounge ?meta",
 	"?math ?physics ?chemistry",
 	"?technology ?programming",
 	"?games ?banana",
-	"And here's a random one generated just for you: ?" + Math.random().toString(36).substr(2, 8),
+	"以及为您随机生成的一个频道： ?" + Math.random().toString(36).substr(2, 8),
 	"",
-	"Formatting:",
-	"Whitespace is preserved, so source code can be pasted verbatim.",
-	"Surround LaTeX with a dollar sign for inline style $\\zeta(2) = \\pi^2/6$, and two dollars for display. $$\\int_0^1 \\int_0^1 \\frac{1}{1-xy} dx dy = \\frac{\\pi^2}{6}$$",
-	"For syntax highlight, wrap the code like: \\`\\`\\`<language> <the code>\\`\\`\\` where <language> is any known programming language.",
+	"格式化：",
+	"空格会被保留，因此可以直接粘贴源代码。",
+	"用单个美元符号包围 LaTeX 代码以实现行内样式 $\\zeta(2) = \\pi^2/6$，用两个美元符号包围以实现显示样式。$$\\int_0^1 \\int_0^1 \\frac{1}{1-xy} dx dy = \\frac{\\pi^2}{6}$$",
+	"要进行语法高亮显示，请将代码包裹在以下格式中：\\\`\\\`\\\`<语言> <代码>\\\`\\\`\\\`，其中<语言>是任何已知的编程语言。",
 	"",
-	"Current Github: https://github.com/hack-chat",
-	"Legacy GitHub: https://github.com/AndrewBelt/hack.chat",
+	"当前 Github：https://github.com/hack-chat",
+	"旧版 Github：https://github.com/AndrewBelt/hack.chat",
 	"",
-	"Bots, Android clients, desktop clients, browser extensions, docker images, programming libraries, server modules and more:",
+	"机器人、安卓客户端、桌面客户端、浏览器扩展、Docker 镜像、编程库、服务器模块等：",
 	"https://github.com/hack-chat/3rd-party-software-list",
 	"",
-	"Server and web client released under the WTFPL and MIT open source license.",
-	"No message history is retained on the hack.chat server."
+	"服务器和 Web 客户端在 WTFPL 和 MIT 开源许可下发布。",
+	"hack.chat 服务器不会保留任何消息历史记录。"
 ].join("\n");
 
 function $(query) {
@@ -173,8 +173,8 @@ var lastSent = [""];
 var lastSentPos = 0;
 
 /**
- * Stores active messages
- * These are messages that can be edited.
+ * 存储活动消息
+ * 这些是可以编辑的消息。
  * @type {{ customId: string, userid: number, sent: number, text: string, elem: HTMLElement }[]}
  */
 var activeMessages = [];
@@ -200,24 +200,24 @@ function addActiveMessage(customId, userid, text, elem) {
 	});
 }
 
-/** Notification switch and local storage behavior **/
+/** 通知开关和本地存储行为 **/
 var notifySwitch = document.getElementById("notify-switch")
 var notifySetting = localStorageGet("notify-api")
-var notifyPermissionExplained = 0; // 1 = granted msg shown, -1 = denied message shown
+var notifyPermissionExplained = 0; // 1 = 已授权消息已显示, -1 = 已拒绝消息已显示
 
-// Initial request for notifications permission
+// 初始化请求通知权限
 function RequestNotifyPermission() {
 	try {
 		var notifyPromise = Notification.requestPermission();
 		if (notifyPromise) {
 			notifyPromise.then(function (result) {
-				console.log("Hack.Chat notification permission: " + result);
+				console.log("Hack.Chat 通知权限: " + result);
 				if (result === "granted") {
 					if (notifyPermissionExplained === 0) {
 						pushMessage({
 							cmd: "chat",
 							nick: "*",
-							text: "Notifications permission granted.",
+							text: "已授予通知权限。",
 							time: null
 						});
 						notifyPermissionExplained = 1;
@@ -228,7 +228,7 @@ function RequestNotifyPermission() {
 						pushMessage({
 							cmd: "chat",
 							nick: "*",
-							text: "Notifications permission denied, you won't be notified if someone @mentions you.",
+							text: "通知权限已被拒绝，如果有人@您，您将不会收到通知。",
 							time: null
 						});
 						notifyPermissionExplained = -1;
@@ -241,68 +241,68 @@ function RequestNotifyPermission() {
 		pushMessage({
 			cmd: "chat",
 			nick: "*",
-			text: "Unable to create a notification.",
+			text: "无法创建通知。",
 			time: null
 		});
-		console.error("An error occured trying to request notification permissions. This browser might not support desktop notifications.\nDetails:")
+		console.error("尝试请求通知权限时发生错误。此浏览器可能不支持桌面通知。\n详细信息：")
 		console.error(error)
 		return false;
 	}
 }
 
-// Update localStorage with value of checkbox
+// 使用复选框的值更新 localStorage
 notifySwitch.addEventListener('change', (event) => {
 	if (event.target.checked) {
 		RequestNotifyPermission();
 	}
 	localStorageSet("notify-api", notifySwitch.checked)
 })
-// Check if localStorage value is set, defaults to OFF
+// 检查 localStorage 值是否已设置，默认为关闭
 if (notifySetting === null) {
 	localStorageSet("notify-api", "false")
 	notifySwitch.checked = false
 }
-// Configure notifySwitch checkbox element
+// 配置 notifySwitch 复选框元素
 if (notifySetting === "true" || notifySetting === true) {
 	notifySwitch.checked = true
 } else if (notifySetting === "false" || notifySetting === false) {
 	notifySwitch.checked = false
 }
 
-/** Sound switch and local storage behavior **/
+/** 声音开关和本地存储行为 **/
 var soundSwitch = document.getElementById("sound-switch")
 var notifySetting = localStorageGet("notify-sound")
 
-// Update localStorage with value of checkbox
+// 使用复选框的值更新 localStorage
 soundSwitch.addEventListener('change', (event) => {
 	localStorageSet("notify-sound", soundSwitch.checked)
 })
-// Check if localStorage value is set, defaults to OFF
+// 检查 localStorage 值是否已设置，默认为关闭
 if (notifySetting === null) {
 	localStorageSet("notify-sound", "false")
 	soundSwitch.checked = false
 }
-// Configure soundSwitch checkbox element
+// 配置 soundSwitch 复选框元素
 if (notifySetting === "true" || notifySetting === true) {
 	soundSwitch.checked = true
 } else if (notifySetting === "false" || notifySetting === false) {
 	soundSwitch.checked = false
 }
 
-// Create a new notification after checking if permission has been granted
+// 在检查是否已授予权限后创建新通知
 function spawnNotification(title, body) {
-	// Let's check if the browser supports notifications
+	// 检查浏览器是否支持通知
 	if (!("Notification" in window)) {
-		console.error("This browser does not support desktop notification");
-	} else if (Notification.permission === "granted") { // Check if notification permissions are already given
-		// If it's okay let's create a notification
+		console.error("此浏览器不支持桌面通知");
+	} else if (Notification.permission === "granted") { // 检查是否已授予通知权限
+		// 如果可以，则创建通知
 		var options = {
 			body: body,
 			icon: "/favicon-96x96.png"
 		};
 		var n = new Notification(title, options);
 	}
-	// Otherwise, we need to ask the user for permission
+	// 否则，我们需要请求用户授予权限
 	else if (Notification.permission !== "denied") {
 		if (RequestNotifyPermission()) {
 			var options = {
@@ -312,23 +312,23 @@ function spawnNotification(title, body) {
 			var n = new Notification(title, options);
 		}
 	} else if (Notification.permission == "denied") {
-		// At last, if the user has denied notifications, and you
-		// want to be respectful, there is no need to bother them any more.
+		// 最后，如果用户拒绝了通知，并且您
+		// 想尊重他们的选择，则无需再打扰他们。
 	}
 }
 
 function notify(args) {
-	// Spawn notification if enabled
+	// 如果启用，则弹出通知
 	if (notifySwitch.checked) {
 		spawnNotification("?" + myChannel + "  —  " + args.nick, args.text)
 	}
 
-	// Play sound if enabled
+	// 如果启用，则播放声音
 	if (soundSwitch.checked) {
 		var soundPromise = document.getElementById("notify-sound").play();
 		if (soundPromise) {
 			soundPromise.catch(function (error) {
-				console.error("Problem playing sound:\n" + error);
+				console.error("播放声音时出现问题：\n" + error);
 			});
 		}
 	}
@@ -336,15 +336,15 @@ function notify(args) {
 
 function join(channel) {
 	if (document.domain == 'hack.chat') {
-		// For https://hack.chat/
+		// 对于 https://hack.chat/
 		ws = new WebSocket('wss://hack.chat/chat-ws');
 	} else {
-		// for local installs
+		// 对于本地安装
 		var protocol = location.protocol === 'https:' ? 'wss:' : 'ws:'
-		// if you changed the port during the server config, change 'wsPath'
-		// to the new port (example: ':8080')
-		// if you are reverse proxying, change 'wsPath' to the new location
-		// (example: '/chat-ws')
+		// 如果您在服务器配置期间更改了端口，请更改 'wsPath'
+		// 为新端口（例如: ':8080'）
+		// 如果您正在使用反向代理，请更改 'wsPath' 为新位置
+		// （例如: '/chat-ws'）
 		var wsPath = ':6060';
 		ws = new WebSocket(protocol + '//' + document.domain + wsPath);
 	}
@@ -357,11 +357,11 @@ function join(channel) {
 			if (location.hash) {
 				myNick = location.hash.substr(1);
 			} else {
-				var newNick = prompt('Nickname:', myNick);
+				var newNick = prompt('昵称：', myNick);
 				if (newNick !== null) {
 					myNick = newNick;
 				} else {
-					// The user cancelled the prompt in some manner
+					// 用户以某种方式取消了提示
 					shouldConnect = false;
 				}
 			}
@@ -377,7 +377,7 @@ function join(channel) {
 
 	ws.onclose = function () {
 		if (wasConnected) {
-			pushMessage({ nick: '!', text: "Server disconnected. Attempting to reconnect. . ." });
+			pushMessage({ nick: '!', text: "服务器连接断开。正在尝试重新连接..." });
 		}
 
 		window.setTimeout(function () {
@@ -449,7 +449,7 @@ var COMMANDS = {
 
 		message.text = newText;
 
-		// Scroll to bottom if necessary
+		// 如果需要，滚动到底部
 		var atBottom = isAtBottom();
 
 		textElem.innerHTML = md.render(newText);
@@ -483,7 +483,7 @@ var COMMANDS = {
 			userAdd(nick);
 		});
 
-		pushMessage({ nick: '*', text: "Users online: " + nicks.join(", ") })
+		pushMessage({ nick: '*', text: "在线用户：" + nicks.join(", ") })
 	},
 
 	onlineAdd: function (args) {
@@ -492,7 +492,7 @@ var COMMANDS = {
 		userAdd(nick);
 
 		if ($('#joined-left').checked) {
-			pushMessage({ nick: '*', text: nick + " joined" });
+			pushMessage({ nick: '*', text: nick + " 加入了聊天" });
 		}
 	},
 
@@ -502,7 +502,7 @@ var COMMANDS = {
 		userRemove(nick);
 
 		if ($('#joined-left').checked) {
-			pushMessage({ nick: '*', text: nick + " left" });
+			pushMessage({ nick: '*', text: nick + " 离开了聊天" });
 		}
 	},
 
@@ -532,7 +532,7 @@ var COMMANDS = {
 }
 
 function pushMessage(args) {
-	// Message container
+	// 消息容器
 	var messageEl = document.createElement('div');
 
 	if (
@@ -558,7 +558,7 @@ function pushMessage(args) {
 		messageEl.classList.add('mod');
 	}
 
-	// Nickname
+	// 昵称
 	var nickSpanEl = document.createElement('span');
 	nickSpanEl.classList.add('nick');
 	messageEl.appendChild(nickSpanEl);
@@ -594,14 +594,14 @@ function pushMessage(args) {
 		nickSpanEl.appendChild(nickLinkEl);
 	}
 
-	// Text
+	// 文本
 	var textEl = document.createElement('p');
 	textEl.classList.add('text');
 	textEl.innerHTML = md.render(args.text);
 
 	messageEl.appendChild(textEl);
 
-	// Scroll to bottom
+	// 滚动到底部
 	var atBottom = isAtBottom();
 	$('#messages').appendChild(messageEl);
 	if (atBottom) {
@@ -680,10 +680,10 @@ $('#footer').onclick = function () {
 }
 
 $('#chatinput').onkeydown = function (e) {
-	if (e.keyCode == 13 /* ENTER */ && !e.shiftKey) {
+	if (e.keyCode == 13 /* 回车键 */ && !e.shiftKey) {
 		e.preventDefault();
 
-		// Submit message
+		// 发送消息
 		if (e.target.value != '') {
 			var text = e.target.value;
 			e.target.value = '';
@@ -696,8 +696,8 @@ $('#chatinput').onkeydown = function (e) {
 
 			updateInputSize();
 		}
-	} else if (e.keyCode == 38 /* UP */) {
-		// Restore previous sent messages
+	} else if (e.keyCode == 38 /* 向上箭头 */) {
+		// 恢复之前发送的消息
 		if (e.target.selectionStart === 0 && lastSentPos < lastSent.length - 1) {
 			e.preventDefault();
 
@@ -711,7 +711,7 @@ $('#chatinput').onkeydown = function (e) {
 
 			updateInputSize();
 		}
-	} else if (e.keyCode == 40 /* DOWN */) {
+	} else if (e.keyCode == 40 /* 向下箭头 */) {
 		if (e.target.selectionStart === e.target.value.length && lastSentPos > 0) {
 			e.preventDefault();
 
@@ -724,18 +724,18 @@ $('#chatinput').onkeydown = function (e) {
 	} else if (e.keyCode == 27 /* ESC */) {
 		e.preventDefault();
 
-		// Clear input field
+		// 清空输入框
 		e.target.value = "";
 		lastSentPos = 0;
 		lastSent[lastSentPos] = "";
 
 		updateInputSize();
 	} else if (e.keyCode == 9 /* TAB */) {
-		// Tab complete nicknames starting with @
+		// 使用 @ 符号开头的昵称进行 Tab 自动补全
 
 		if (e.ctrlKey) {
-			// Skip autocompletion and tab insertion if user is pressing ctrl
-			// ctrl-tab is used by browsers to cycle through tabs
+			// 如果用户按下 Ctrl 键，则跳过自动补全和插入 Tab
+			// Ctrl-Tab 用于在浏览器选项卡之间循环
 			return;
 		}
 		e.preventDefault();
@@ -748,7 +748,7 @@ $('#chatinput').onkeydown = function (e) {
 
 		if (index >= 0) {
 			var stub = text.substring(index + 1, pos).toLowerCase();
-			// Search for nick beginning with stub
+			// 搜索以 stub 开头的昵称
 			var nicks = onlineUsers.filter(function (nick) {
 				return nick.toLowerCase().indexOf(stub) == 0
 			});
@@ -761,14 +761,14 @@ $('#chatinput').onkeydown = function (e) {
 			}
 		}
 
-		// Since we did not insert a nick, we insert a tab character
+		// 因为我们没有插入昵称，所以我们插入一个 Tab 字符
 		if (!autocompletedNick) {
 			insertAtCursor('\t');
 		}
 	}
 }
 
-// from https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+// 来自 https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 function checkIsMobileOrTablet() {
 	let check = false;
 	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -798,7 +798,7 @@ $('#chatinput').oninput = function () {
 
 updateInputSize();
 
-/* sidebar */
+/* 侧边栏 */
 
 $('#sidebar').onmouseenter = $('#sidebar').ontouchstart = function (e) {
 	$('#sidebar-content').classList.remove('hidden');
@@ -821,12 +821,12 @@ $('#sidebar').onmouseleave = document.ontouchstart = function (event) {
 }
 
 $('#clear-messages').onclick = function () {
-	// Delete children elements
+	// 删除子元素
 	var messages = $('#messages');
 	messages.innerHTML = '';
 }
 
-// Restore settings from localStorage
+// 从 localStorage 中恢复设置
 
 if (localStorageGet('pin-sidebar') == 'true') {
 	$('#pin-sidebar').checked = true;
@@ -885,7 +885,7 @@ $('#allow-imgur').onchange = function (e) {
 	allowImages = enabled;
 }
 
-// User list
+// 用户列表
 var onlineUsers = [];
 var ignoredUsers = [];
 
@@ -938,7 +938,7 @@ function userIgnore(nick) {
 	ignoredUsers.push(nick);
 }
 
-/* color scheme switcher */
+/* 配色方案切换器 */
 
 var schemes = [
 	'android',
@@ -1022,7 +1022,7 @@ function sortArrayCaseInsenstive(array) {
 }
 
 
-// Add scheme options to dropdown selector
+// 将配色方案选项添加到下拉选择器
 sortArrayCaseInsenstive(schemes).forEach(function (scheme) {
 	var option = document.createElement('option');
 	option.textContent = scheme;
@@ -1045,7 +1045,7 @@ $('#highlight-selector').onchange = function (e) {
 	setHighlight(e.target.value);
 }
 
-// Load sidebar configuration values from local storage if available
+// 如果可用，从本地存储中加载侧边栏配置值
 if (localStorageGet('scheme')) {
 	setScheme(localStorageGet('scheme'));
 }
@@ -1057,7 +1057,7 @@ if (localStorageGet('highlight')) {
 $('#scheme-selector').value = currentScheme;
 $('#highlight-selector').value = currentHighlight;
 
-/* main */
+/* 主程序 */
 
 if (myChannel == '') {
 	pushMessage({ text: frontpage });
